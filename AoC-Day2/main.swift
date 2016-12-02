1d62b92afe786a955999a588a5e6fa0eb340455c
Gabriel Bretschner <software@kanedo.net>
2016-12-02 08:26:02 +0100

    added binary for day2

---
+++ AoC-Day2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AoC-Day2
+//
+//  Created by Gabriel Bretschner on 02.12.16.
+//  Copyright © 2016 Gabriel Bretschner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
