9f992fbe75cba5d7241326c0c14e5039577ebca7
Gabriel Bretschner <software@kanedo.net>
2016-12-04 06:41:39 +0100

    added day4

---
+++ AoC-Day4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AoC-Day4
+//
+//  Created by Gabriel Bretschner on 04.12.16.
+//  Copyright © 2016 Gabriel Bretschner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
